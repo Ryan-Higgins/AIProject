30cd923dde1b877e3eb93c501408841946156d86
Ryan Higgins <c16381886@mydit.ie>
2019-02-26 11:00:33 +0000

    Started

---
+++ AIProject/Assets/Scripts/Pursue.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pursue : SteeringBehaviour
+{
+    public Boid target;
+
+    Vector3 targetPos;
+
+    public void OnDrawGizmos()
+    {
+        if (Application.isPlaying)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(transform.position, targetPos);
+        }
+    }
+
+    public override Vector3 Calculate()
+    {
+        float dist = Vector3.Distance(target.transform.position, transform.position);
+        float time = dist / boid.maxSpeed;
+        float beforeY = transform.position.y;
+        targetPos = target.transform.position + (target.velocity * time);
+        targetPos.y = beforeY;
+
+        return boid.SeekForce(targetPos);
+    }
+}
